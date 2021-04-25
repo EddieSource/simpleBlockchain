8319aeead82ad37eb76ba4e6e986d00047991002
Xianhui Zhu <xz2715@nyu.edu>
2021-04-25 12:45:44 -0400

    create a block

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,5 @@
+# create a simple block chain
+
 import datetime
 import hashlib
 import json
@@ -8,6 +10,18 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        # create a block using a dictionary of generally 4 central keys
+        # use this after a block is mined: need the proof based on 'proof of work'
+        # can create more by adding the 'data' key (will use this in next module)
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': datetime.datetime.now(), # return year,month,day,hour,min,sec
+                 'proof': proof,
+                 'previous_hash': previous_hash
+                 }
+        self.chain.append(block)
+        return block
 
 
 #part 2 - Mining our Blockchain
