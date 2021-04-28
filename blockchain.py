13e92a80816419d4db0fd652495d9c3d70ae32d1
Xianhui Zhu <xz2715@nyu.edu>
2021-04-28 13:22:35 -0400

    get previous block

--- blockchain.py
+++ blockchain.py
@@ -13,7 +13,7 @@ class Blockchain:
 
     def create_block(self, proof, previous_hash):
         # create a block using a dictionary of generally 4 central keys
-        # use this after a block is mined: need the proof based on 'proof of work'
+        # use this after a block is mined: need the proof based on 'proof of work': define the problem
         # can create more by adding the 'data' key (will use this in next module)
         block = {'index': len(self.chain) + 1,
                  'timestamp': datetime.datetime.now(), # return year,month,day,hour,min,sec
@@ -23,5 +23,8 @@ class Blockchain:
         self.chain.append(block)
         return block
 
+    def get_previous_block(self):
+        #give the last block of the chain
+        return self.chain[-1]
 
 #part 2 - Mining our Blockchain
