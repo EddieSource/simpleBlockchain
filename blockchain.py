16b935cd8e645846df104011b26cc835936c8e0e
Xianhui Zhu <xz2715@nyu.edu>
2021-05-06 14:01:51 -0400

    add is_chain_valid api

--- blockchain.py
+++ blockchain.py
@@ -129,6 +129,16 @@ def get_chain():
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
+@app.route('/is_valid', methods=["GET"])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Problem occurs. Blockchain is NOT valid'}
+
+    return jsonify(response), 200
+
 # Running the app on http://127.0.0.1:5000/
 # use postman to test
 app.run(host = '0.0.0.0', port = 5000)
